bf3ef500ab446586a0b4c6264759c36d839cc1d9
artgameweekend <artgameweekend@gmail.com>
2011-04-23 07:04:44 +0200

    Properties

--- Assets/My_assets/Scripts/Room.js
+++ Assets/My_assets/Scripts/Room.js
@@ -8,8 +8,11 @@ class C_Room extends MonoBehaviour
 	public var c_Shape : int;		// 0 : Square, 1 : Ball, 2 : Cylinder
 	public var c_Color : int;		// ???
 	
-	function c_Room_Constructeur()
+	function c_Room_Constructeur(p_X : int, p_Y : int, p_Shape : int, p_Color : int)
 	{
-		
+		c_X = p_X;
+		c_Y = p_Y;
+		c_Shape = p_Shape;
+		c_Color = p_Color;
 	}
 }
